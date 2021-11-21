4d9abfb88e29b7b1340e7ef66af35c53e63ccac6
Julian Jozic <jjozic98@gmail.com>
2021-11-21 12:18:32 +0100

    removed default assets, added AirlineTicket component with static props

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <AirlineTicket
+      bgColor="lightblue"
+      fontSize=12
+      fontColor="gold"
+      passengerName="Max Mustermann"
+    />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import AirlineTicket from './components/AirlineTicket.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
+    AirlineTicket,
   },
 };
 </script>
